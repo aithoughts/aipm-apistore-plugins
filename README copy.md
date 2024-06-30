28c96414b7ddcbaad587cda01a28a77348860657
ai.flyingwheel <ai.flyingwheel@gmail.com>
2024-06-30 12:28:06 +0800

    commits

--- README copy.md
+++
@@ -1,3 +0,0 @@
-<h1>OpenAI Plugins Directory<br/><sup>AiPMChat Plugin</sup></h1>
-
-Proxy for selected ChatGPT plugins from community
--- src/syncList.ts
+++ src/syncList.ts
@@ -11,8 +11,12 @@ interface PluginItem {
 }
 const PluginList: PluginItem[] = [
   {
-    manifest:
-      'https://raw.githubusercontent.com/aipmhub/chat-plugin-serper/main/public/manifest.json',
+    manifest: 'https://openai-collections.chat-plugin.lobehub.com/txyz/manifest.json',
+    path: 'ResearchAI',
+    tags: ['research paper', 'search'],
+  },
+  {
+    manifest: 'https://chat-plugin-apistore.theforage.cn/serper/manifest.json',
     path: 'serper',
     tags: ['web', 'search'],
   },
